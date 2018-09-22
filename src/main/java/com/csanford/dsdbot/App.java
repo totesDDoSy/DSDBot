e60ca0ef6b631f48763fa5f0bf125b9037285035
Cody Sanford <cody.b.sanford@gmail.com>
2018-09-21 22:45:40 -0600

    Now with updating messages.

--- src/main/java/com/csanford/dsdbot/App.java
+++ src/main/java/com/csanford/dsdbot/App.java
@@ -7,6 +7,7 @@ import com.ullink.slack.simpleslackapi.SlackUser;
 import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
 import com.ullink.slack.simpleslackapi.listeners.SlackMessageDeletedListener;
 import com.ullink.slack.simpleslackapi.listeners.SlackMessagePostedListener;
+import com.ullink.slack.simpleslackapi.listeners.SlackMessageUpdatedListener;
 import com.ullink.slack.simpleslackapi.replies.SlackMessageReply;
 import java.io.IOException;
 import java.util.HashMap;
@@ -24,6 +25,7 @@ import net.dv8tion.jda.core.entities.Message.MentionType;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageDeleteEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.events.message.MessageUpdateEvent;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
@@ -35,11 +37,35 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 public class App extends ListenerAdapter
 {
 
+    private static String convertSlackMessage( String slackMessage, String sender, SlackSession session )
+    {
+	StringBuilder discordMessage = new StringBuilder();
+
+	// Make a map for user IDs to names
+	Map<String, String> usersMap = session.getUsers().stream()
+		.collect( Collectors.toMap( user -> user.getId(), user -> user.getUserName() ) );
+	for ( Map.Entry<String, String> entry : usersMap.entrySet() )
+	{
+	    slackMessage = slackMessage
+		    .replaceAll( entry.getKey(), entry.getValue() )
+		    .replaceAll( "[<>]", "" );
+	}
+
+	if ( sender != null )
+	{
+	    discordMessage.append( "**" ).append( sender ).append( "**: " );
+	}
+
+	discordMessage.append( slackMessage );
+
+	return discordMessage.toString();
+    }
+
     private final SlackSession slackSession;
-    private static Map< String, Message > stodMessages = new HashMap<>();
-    private static Queue<String> stodHistory = new ConcurrentLinkedQueue<>();
-    private static Map< Long, String > dtosMessages = new HashMap<>();
-    private static Queue< Long > dtosHistory = new ConcurrentLinkedQueue<>();
+    private static Map< String, Message> stodMessages = new HashMap<>();
+    private static Queue< String> stodHistory = new ConcurrentLinkedQueue<>();
+    private static Map< Long, String> dtosMessages = new HashMap<>();
+    private static Queue< Long> dtosHistory = new ConcurrentLinkedQueue<>();
 
     public App( SlackSession slackSession )
     {
@@ -49,61 +75,63 @@ public class App extends ListenerAdapter
     public static void main( String[] args ) throws LoginException, IllegalArgumentException, InterruptedException, RateLimitedException, IOException
     {
 	// Setup Slack API Connection
-	SlackSession session = SlackSessionFactory.getSlackSessionBuilder( SecureConstants.SLACK_TOKEN ).build();
-	session.connect();
+	SlackSession slackSession = SlackSessionFactory.getSlackSessionBuilder( SecureConstants.SLACK_TOKEN ).build();
+	slackSession.connect();
 
 	// Setup Discord API connection
 	JDA jda = new JDABuilder( AccountType.BOT )
 		.setToken( SecureConstants.DISCORD_TOKEN ).buildBlocking();
-	jda.addEventListener( new App( session ) );
+	jda.addEventListener( new App( slackSession ) );
 
 	SlackMessagePostedListener slackMessagePostedListener
-		= ( event, session1 ) ->
+		= ( event, session ) ->
 	{
 	    // Slack Message Listener
 	    String slackMessage = event.getMessageContent();
-	    String selfId = session1.sessionPersona().getId();
+	    String selfId = session.sessionPersona().getId();
 	    SlackUser sender = event.getSender();
-	    if ( !session1.sessionPersona().getId().equals( sender.getId() )
+	    if ( !session.sessionPersona().getId().equals( sender.getId() )
 		    && slackMessage.contains( "@" + selfId ) )
 	    {
 		// If the bot didn't send the message and was mentioned, send a discord message
-		StringBuilder discordMessage = new StringBuilder();
-
-		// Make a map for user IDs to names
-		Map<String, String> usersMap = session1.getUsers().stream()
-			.collect( Collectors.toMap( user -> user.getId(), user -> user.getUserName() ) );
-		for ( Map.Entry<String, String> entry : usersMap.entrySet() )
-		{
-		    slackMessage = slackMessage
-			    .replaceAll( entry.getKey(), entry.getValue() )
-			    .replaceAll( "[<>]", "" );
-		}
-
-		discordMessage.append( "**" ).append( sender.getUserName() ).append( "**: " );
-		discordMessage.append( slackMessage );
+		String discordMessage = convertSlackMessage( slackMessage, sender.getUserName(), session );
 
 		// Send the discord message
 		Message message = jda.getTextChannelsByName( Constants.DISCORD_CHANNEL, true ).get( 0 )
-			.sendMessage( discordMessage.toString() ).complete();
-		
+			.sendMessage( discordMessage ).complete();
+
 		saveDiscordMessage( event.getTimestamp(), message );
 	    }
 	};
-	
-	SlackMessageDeletedListener slackMessageDeletedListener = ( event, session2 ) ->
+
+	SlackMessageDeletedListener slackMessageDeletedListener = ( event, session ) ->
 	{
 	    // Message deleted on slack
-	    String selfId = session2.sessionPersona().getId();
 	    Message discordMsg = stodMessages.remove( event.getMessageTimestamp() );
 	    if ( discordMsg != null )
 	    {
 		discordMsg.delete().complete();
 	    }
 	};
-	
-	session.addMessagePostedListener( slackMessagePostedListener );
-	session.addMessageDeletedListener( slackMessageDeletedListener );
+
+	SlackMessageUpdatedListener slackMessageUpdatedListener = ( event, session ) ->
+	{
+	    Message message = stodMessages.get( event.getMessageTimestamp() );
+	    if ( message != null )
+	    {
+		String discordMsg = message.getContentDisplay();
+		StringBuilder slackMessage = new StringBuilder();
+		slackMessage.append( discordMsg.subSequence( 0, discordMsg.indexOf( ' ' ) + 1 ) );
+		slackMessage.append( convertSlackMessage( event.getNewMessage(), null, session ) );
+
+		Message newMessage = message.editMessage( slackMessage.toString() ).complete();
+		// do stuff with history?
+	    }
+	};
+
+	slackSession.addMessagePostedListener( slackMessagePostedListener );
+	slackSession.addMessageDeletedListener( slackMessageDeletedListener );
+	slackSession.addMessageUpdatedListener( slackMessageUpdatedListener );
     }
 
     @Override
@@ -116,18 +144,16 @@ public class App extends ListenerAdapter
 	if ( discordMessage.isMentioned( selfMember, MentionType.USER ) && !messageAuthor.isBot() )
 	{
 	    // If the bot was mentioned by not itself, send a slack message
-	    StringBuilder slackMessage = new StringBuilder();
+	    String slackMessage = convertDiscordMessage( messageAuthor.getName(),
+		    discordMessage.getContentDisplay() );
 
-	    slackMessage.append( "*" ).append( messageAuthor.getName() ).append( "*: ");
-	    slackMessage.append( discordMessage.getContentDisplay() );
-	    
 	    SlackChannel channel = slackSession.findChannelByName( Constants.SLACK_CHANNEL );
-	    SlackMessageHandle<SlackMessageReply> sendMessage = 
-		    slackSession.sendMessage( channel, slackMessage.toString() );
+	    SlackMessageHandle<SlackMessageReply> sendMessage
+		    = slackSession.sendMessage( channel, slackMessage );
 	    saveSlackMessage( event.getMessageIdLong(), sendMessage.getReply().getTimestamp() );
 	}
     }
-    
+
     @Override
     public void onMessageDelete( MessageDeleteEvent event )
     {
@@ -139,9 +165,24 @@ public class App extends ListenerAdapter
 	    slackSession.deleteMessage( timestamp, channel );
 	}
     }
-    
+
+    @Override
+    public void onMessageUpdate( MessageUpdateEvent event )
+    {
+	String timestamp = dtosMessages.get( event.getMessageIdLong() );
+	if ( timestamp != null )
+	{
+	    String author = event.getAuthor().getName();
+	    String discordMessage = event.getMessage().getContentDisplay();
+	    String slackMessage = convertDiscordMessage( author, discordMessage );
+	    SlackChannel channel = slackSession.findChannelByName( Constants.SLACK_CHANNEL );
+	    slackSession.updateMessage( timestamp, channel, slackMessage );
+	}
+    }
+
     /**
      * Save a discord message with a slack timestamp.
+     *
      * @param timestamp Timestamp of slack message.
      * @param message Discord message object.
      */
@@ -155,7 +196,7 @@ public class App extends ListenerAdapter
 	}
 	stodMessages.put( timestamp, message );
     }
-    
+
     /**
      * Save a slack message with a discord message long id.
      * @param messageId The Discord long message id.
@@ -170,5 +211,14 @@ public class App extends ListenerAdapter
 	    dtosMessages.remove( dtosHistory.remove() );
 	}
 	dtosMessages.put( messageId, timestamp );
+    }
+
+    private String convertDiscordMessage( String name, String contentDisplay )
+    {
+	StringBuilder slackMessage = new StringBuilder();
+
+	slackMessage.append( "*" ).append( name ).append( "*: " );
+	slackMessage.append( contentDisplay );
+	return slackMessage.toString();
     }
 }
